c76c3a4b01226030c9436aab3ca921384bfa363b
Alex <alexandr.yakimov.heaven@gmail.com>
2025-07-09 20:31:23 +0800

    Реализован компонент Navbar с навигационным меню приложения, добавлены категории без роутинга.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+function Navbar() {
+    return (
+        <>
+            <nav className="navbar">
+                <ul className="navbar__list">
+                    <li className="navbar__list__item"><img className="navbar__list__image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxSHOwFfTAvVk9JnKR7e1q3eebM9c5mpMFy2IYQC3wxFEK8Ijc4vNeTTLym-ncX6HBxKw&usqp=CAU" /><a href="#" className="navbar__list__link">Готовая еда</a></li>
+                    <li className="navbar__list__item"><img className="navbar__list__image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxSHOwFfTAvVk9JnKR7e1q3eebM9c5mpMFy2IYQC3wxFEK8Ijc4vNeTTLym-ncX6HBxKw&usqp=CAU" /><a href="#" className="navbar__list__link">Морозилка</a></li>
+                    <li className="navbar__list__item"><img className="navbar__list__image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxSHOwFfTAvVk9JnKR7e1q3eebM9c5mpMFy2IYQC3wxFEK8Ijc4vNeTTLym-ncX6HBxKw&usqp=CAU" /><a href="#" className="navbar__list__link">Овощи и фрукты</a></li>
+                    <li className="navbar__list__item"><img className="navbar__list__image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxSHOwFfTAvVk9JnKR7e1q3eebM9c5mpMFy2IYQC3wxFEK8Ijc4vNeTTLym-ncX6HBxKw&usqp=CAU" /><a href="#" className="navbar__list__link">Молочная продукция</a></li>
+                    <li className="navbar__list__item"><img className="navbar__list__image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxSHOwFfTAvVk9JnKR7e1q3eebM9c5mpMFy2IYQC3wxFEK8Ijc4vNeTTLym-ncX6HBxKw&usqp=CAU" /><a href="#" className="navbar__list__link">Баккалея</a></li>
+                    <li className="navbar__list__item"><img className="navbar__list__image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxSHOwFfTAvVk9JnKR7e1q3eebM9c5mpMFy2IYQC3wxFEK8Ijc4vNeTTLym-ncX6HBxKw&usqp=CAU" /><a href="#" className="navbar__list__link">Хлеб</a></li>
+                    <li className="navbar__list__item"><img className="navbar__list__image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxSHOwFfTAvVk9JnKR7e1q3eebM9c5mpMFy2IYQC3wxFEK8Ijc4vNeTTLym-ncX6HBxKw&usqp=CAU" /><a href="#" className="navbar__list__link">Мясо и рыба</a></li>
+                    <li className="navbar__list__item"><img className="navbar__list__image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxSHOwFfTAvVk9JnKR7e1q3eebM9c5mpMFy2IYQC3wxFEK8Ijc4vNeTTLym-ncX6HBxKw&usqp=CAU" /><a href="#" className="navbar__list__link">Вода и напитки</a></li>
+                    <li className="navbar__list__item"><img className="navbar__list__image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxSHOwFfTAvVk9JnKR7e1q3eebM9c5mpMFy2IYQC3wxFEK8Ijc4vNeTTLym-ncX6HBxKw&usqp=CAU" /><a href="#" className="navbar__list__link">Сладкое</a></li>
+                    <li className="navbar__list__item"><img className="navbar__list__image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxSHOwFfTAvVk9JnKR7e1q3eebM9c5mpMFy2IYQC3wxFEK8Ijc4vNeTTLym-ncX6HBxKw&usqp=CAU" /><a href="#" className="navbar__list__link">Снеки</a></li>
+                    <li className="navbar__list__item"><img className="navbar__list__image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxSHOwFfTAvVk9JnKR7e1q3eebM9c5mpMFy2IYQC3wxFEK8Ijc4vNeTTLym-ncX6HBxKw&usqp=CAU" /><a href="#" className="navbar__list__link">Для детей</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
