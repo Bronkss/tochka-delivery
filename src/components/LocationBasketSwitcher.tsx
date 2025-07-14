dabbbf382e8b3fd7d7fd30ee46891b19e8afbf69
Alex <alexandr.yakimov.heaven@gmail.com>
2025-07-14 18:39:22 +0800

    Добавлен компонент LocationBasketSwitcher.tsx в котором будет реализация Геолокации с выбором адреса доставки (через Yandex Map API). И корзина покупок. Компонент YandexMaps.tsx реализован частично, карта отображается, но на ней нет функционала.

---
+++ src/components/LocationBasketSwitcher.tsx
@@ -0,0 +1,14 @@
+import YandexMaps from "./YandexMaps.tsx";
+
+
+function LocationBasketSwitcher() {
+    return (
+        <>
+            <div className="location-basket-switcher">
+                <YandexMaps />
+            </div>
+        </>
+    )
+}
+
+export default LocationBasketSwitcher;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    exclude: ['ymaps3', '@yandex/ymaps3-types']
+  },
+  resolve: {
+    alias: {
+      'ymaps3': './src/types/ymaps3.d.ts' // Путь к вашим декларациям
+    }
+  }
+});
